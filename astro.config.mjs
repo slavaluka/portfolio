425b774860256609211aca7a3d75c8864beb75a5
Sláva Lukianchuk <work.lukianchuk@gmail.com>
2026-01-07 00:36:49 +0100

    feat: meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://slvlkn.cc',
   output: 'server',
   adapter: vercel({}),
 

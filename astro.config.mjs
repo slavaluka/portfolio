40a52dd727bcc985e0ce3b7df91398c6b76c058c
Sláva Lukianchuk <work.lukianchuk@gmail.com>
2026-01-06 16:21:45 +0100

    chore: font clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,15 +17,6 @@ export default defineConfig({
         subsets: ['latin'],
         fallbacks: ['system-ui', 'sans-serif'],
       },
-      {
-        name: 'Inter',
-        cssVariable: '--font-inter',
-        provider: fontProviders.google(),
-        weights: [400, 500, 600, 700],
-        styles: ['normal'],
-        subsets: ['latin'],
-        fallbacks: ['system-ui', 'sans-serif'],
-      },
     ],
   },
   vite: {
